c1c749a45a5318a9c3ee20eccb0a15ff3a1c5bed
Jonathan Boyer <jbsilverdragons@gmail.com>
2017-02-23 00:12:19 -0600

    Update unit test helper classes

--- JBoyerLibaray/UnitTests/Database/FakeParameter.cs
+++ JBoyerLibaray/UnitTests/Database/FakeParameter.cs
@@ -9,6 +9,53 @@ namespace JBoyerLibaray.UnitTests.Database
 {
     public class FakeParameter : IDbDataParameter
     {
+        public FakeParameter() : this("", null) { }
+
+        public FakeParameter(string parameterName, DbType dbType) : this(parameterName, dbType, 0) { }
+
+        public FakeParameter(string parameterName, object value)
+        {
+            int size = 0;
+            DbType dbType;
+            if (!DbType.TryParse(value.GetType().Name, out dbType))
+            {
+                dbType = DbType.String;
+            }
+
+            if (dbType == DbType.String && value is String)
+            {
+                size = (value as String).Length;
+            }
+            
+            construtorLogic(parameterName, dbType, size, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, value);
+        }
+
+        public FakeParameter(string parameterName, DbType dbType, int size) : this(parameterName, dbType, size, "") { }
+
+        public FakeParameter(string parameterName, DbType dbType, int size, string sourceColumn)
+        {
+            construtorLogic(parameterName, dbType, size, ParameterDirection.Input, false, 0, 0, sourceColumn, DataRowVersion.Current, null);
+        }
+
+        public FakeParameter(string parameterName, DbType dbType, int size, ParameterDirection direction, bool isNullable, byte precision, byte scale, string sourceColumn, DataRowVersion sourceVersion, object value)
+        {
+            construtorLogic(parameterName, dbType, size, direction, isNullable, precision, scale, sourceColumn, sourceVersion, value);
+        }
+
+        private void construtorLogic(string parameterName, DbType dbType, int size, ParameterDirection direction, bool isNullable, byte precision, byte scale, string sourceColumn, DataRowVersion sourceVersion, object value)
+        {
+            ParameterName = parameterName;
+            DbType = dbType;
+            Size = size;
+            Direction = direction;
+            IsNullable = isNullable;
+            Precision = precision;
+            Scale = scale;
+            SourceColumn = sourceColumn;
+            SourceVersion = sourceVersion;
+            Value = value;
+        }
+
         public DbType DbType { get; set; }
 
         public ParameterDirection Direction { get; set; }
@@ -27,12 +74,6 @@ namespace JBoyerLibaray.UnitTests.Database
 
         public object Value { get; set; }
 
-        public bool IsNullable
-        {
-            get
-            {
-                return true;
-            }
-        }
+        public bool IsNullable { get; private set; }
     }
 }
--- JBoyerLibaray/UnitTests/Database/FakeParameterCollection.cs
+++ JBoyerLibaray/UnitTests/Database/FakeParameterCollection.cs
@@ -8,191 +8,60 @@ using System.Threading.Tasks;
 
 namespace JBoyerLibaray.UnitTests.Database
 {
-    public class FakeParameterCollection : IDataParameterCollection
+    public class FakeParameterCollection : List<IDbDataParameter>, IDataParameterCollection
     {
-        #region Private Variables
+        public FakeParameterCollection() : base() { }
 
-        private List<IDataParameter> _parameters;
+        public FakeParameterCollection(int capacity) : base(capacity) { }
 
-        #endregion
+        public FakeParameterCollection(IEnumerable<IDbDataParameter> collection) : base(collection) { }
 
-        #region Public Properties
-
-        public int Count
+        public IDbDataParameter this[string parameterName]
         {
             get
             {
-                return _parameters.Count();
+                return this.Where(p => p.ParameterName == parameterName).FirstOrDefault();
+            }
+            set
+            {
+                var parameter = this.Where(p => p.ParameterName == parameterName).FirstOrDefault();
+                if (parameter == null)
+                {
+                    throw new IndexOutOfRangeException(String.Format(
+                        "An IDbDataParameter with ParameterName '{0}' is not contained by this FakeParameterCollection.",
+                        parameterName
+                    ));
+                }
+
+                parameter = value;
             }
         }
 
-        public bool IsSynchronized
+        object IDataParameterCollection.this[string parameterName]
         {
             get
             {
-                return false;
+                return this[parameterName];
             }
-        }
-
-        public object SyncRoot
-        {
-            get
+            set
             {
-                return null;
+                throw new Exception();
             }
         }
-
-        public System.Collections.IEnumerator GetEnumerator()
-        {
-            return _parameters.GetEnumerator();
-        }
-
-        public bool IsFixedSize
-        {
-            get
-            {
-                return false;
-            }
-        }
-
-        public bool IsReadOnly
-        {
-            get
-            {
-                return false;
-            }
-        }
-
-        #endregion
-
-        #region Constructor
-
-        public FakeParameterCollection()
-        {
-            _parameters = new List<IDataParameter>();
-        }
-
-        #endregion
-
-        #region Public Methods
 
         public bool Contains(string parameterName)
         {
-            return _parameters.Select(p => p.ParameterName).Contains(parameterName);
+            return this.Select(p => p.ParameterName).Contains(parameterName);
         }
 
         public int IndexOf(string parameterName)
         {
-            return _parameters.Select(p => p.ParameterName).ToList().IndexOf(parameterName);
+            return this.Select(p => p.ParameterName).ToList().IndexOf(parameterName);
         }
 
         public void RemoveAt(string parameterName)
         {
-            if (!Contains(parameterName))
-            {
-                return;
-            }
-
-            var parameter = _parameters.Where(p => p.ParameterName == parameterName).FirstOrDefault();
-            _parameters.Remove(parameter);
+            this.RemoveAt(IndexOf(parameterName));
         }
-
-        public object this[string parameterName]
-        {
-            get
-            {
-                if (!Contains(parameterName))
-                {
-                    return null;
-                }
-
-                return _parameters.Where(p => p.ParameterName == parameterName).Select(p => p.Value).FirstOrDefault();
-            }
-            set
-            {
-                if (!Contains(parameterName))
-                {
-                    throw ExceptionHelper.CreateArgumentException(() => parameterName, "Parameter not in list");
-                }
-
-                var parameter = _parameters.Where(p => p.ParameterName == parameterName).FirstOrDefault();
-                parameter.Value = value;
-            }
-        }
-
-        public void Clear()
-        {
-            _parameters.Clear();
-        }
-
-        public bool Contains(object value)
-        {
-            return _parameters.Select(p => p.Value).Contains(value);
-        }
-
-        public int IndexOf(object value)
-        {
-            return _parameters.Select(p => p.Value).ToList().IndexOf(value);
-        }
-
-        public int Add(object value)
-        {
-            var parameter = value as IDataParameter;
-            if (parameter == null)
-            {
-                throw ExceptionHelper.CreateArgumentOutOfRangeException(() => value, "value is not a data parameter", value);
-            }
-
-            _parameters.Add(parameter);
-
-            return _parameters.IndexOf(parameter);
-        }
-
-        public void Insert(int index, object value)
-        {
-            var parameter = value as IDataParameter;
-            if (parameter == null)
-            {
-                throw ExceptionHelper.CreateArgumentOutOfRangeException(() => value, "value is not a data parameter", value);
-            }
-
-            _parameters.Insert(index, parameter);
-        }
-
-        public void Remove(object value)
-        {
-            var parameter = value as IDataParameter;
-            if (parameter == null)
-            {
-                throw ExceptionHelper.CreateArgumentOutOfRangeException(() => value, "value is not a data parameter", value);
-            }
-
-            _parameters.Remove(parameter);
-        }
-
-        public void RemoveAt(int index)
-        {
-            _parameters.RemoveAt(index);
-        }
-
-        public object this[int index]
-        {
-            get
-            {
-                return _parameters[index].Value;
-            }
-            set
-            {
-                _parameters[index].Value = value;
-            }
-        }
-
-        public void CopyTo(Array array, int index)
-        {
-            throw new NotImplementedException();
-        }
-
-        #endregion
-
     }
 }
