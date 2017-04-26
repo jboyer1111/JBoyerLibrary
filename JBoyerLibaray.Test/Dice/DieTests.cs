9614573c7acd73b0232fbc58f1a8aea8aca95127
Jonathan Boyer <jboyer@nationalcorp.com>
2017-04-26 09:29:17 -0500

    Made Unit Tests for Dice

---
+++ JBoyerLibaray.Test/Dice/DieTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JBoyerLibaray.Dice
+{
+    [TestClass]
+    public class DieTests
+    {
+
+        [TestMethod]
+        public void Die_ConstructorOne()
+        {
+            // Arrange
+
+            // Act
+            new Die();
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void Die_ConstructorTwo()
+        {
+            // Arrange
+
+            // Act
+            new Die(2);
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void Die_RollKeepsValuesInRange()
+        {
+            // Arrange
+            var die = new Die();
+            List<int> results = new List<int>(2000);
+
+            // Act
+            for (int i = 0; i < 2000; i++)
+            {
+                results.Add(die.Roll());
+            }
+
+            // Assert
+            Assert.IsFalse(results.Any(r => r < 1 || r > 6));
+        }
+
+        [TestMethod]
+        public void Die_RollsAreWellDistributed()
+        {
+            // Arrange
+            var die = new Die();
+            List<int> results = new List<int>(2000);
+
+            // Act
+            for (int i = 0; i < 2000; i++)
+            {
+                results.Add(die.Roll());
+            }
+
+            var buckets = (
+                from r in results
+                group r by r into b
+                select new {
+                   Group = b.First(),
+                   Count = b.Count()
+                }
+            ).ToList();
+
+            // Assert
+            var distributedValue = (results.Count() + 0.000) / (buckets.Count() + 0.000);
+
+            Assert.IsTrue(buckets.Any(b => b.Count < distributedValue - 10 || b.Count > distributedValue + 10 ));
+        }
+
+    }
+}
--- JBoyerLibaray/Dice/Die.cs
+++ JBoyerLibaray/Dice/Die.cs
@@ -8,9 +8,15 @@ namespace JBoyerLibaray.Dice
 {
     public class Die
     {
+        #region Privte Variabls
+
         protected int _sides;
         protected Random _rand;
-        
+
+        #endregion
+
+        #region Constructor
+
         public Die() : this(6) { }
 
         public Die(int sides)
@@ -19,9 +25,15 @@ namespace JBoyerLibaray.Dice
             _rand = new Random((int)DateTime.Now.Ticks);
         }
 
+        #endregion
+
+        #region Public Methods
+
         public int Roll()
         {
             return _rand.Next(0, _sides) + 1;
         }
+
+        #endregion
     }
 }
